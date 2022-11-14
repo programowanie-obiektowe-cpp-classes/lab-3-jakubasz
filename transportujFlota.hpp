6892ad08306e385573da8a8de740479d7e15969e
jakubasz <01146413@pw.edu.pl>
2022-11-14 22:18:31 +0100

    Master

--- transportujFlota.hpp
+++ transportujFlota.hpp
@@ -4,6 +4,17 @@
 
 unsigned int transportujFlota(unsigned int towar)
 {
-    // Twoja implementacja tutaj
-    return 0;
+    int licznik = 0;
+    int liczba  = 0;
+    while (licznik < towar && towar != 0) {
+        Stocznia stocznia{};
+        Statek*  s1  = stocznia();
+        int      t   = s1->transportuj();
+        licznik      = licznik + t;
+        Zaglowiec* z = dynamic_cast< Zaglowiec* >(s1);
+        if (z != 0)
+            liczba = ++liczba;
+        delete s1;
+    }
+    return liczba;
 }
